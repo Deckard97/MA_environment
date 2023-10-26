7c4d5fd9d8d58182762b95f5991a1a5247be1065
Maximilian Stumpf <maximilian.stumpf@adesso-service.com>
2023-10-26 10:35:29 +0200

    finalized db filling

--- additional/fill_db.py
+++ additional/fill_db.py
@@ -5,11 +5,14 @@ from faker import Faker
 fake = Faker()
 fake.seed_instance(0)
 
+# Seed the random number generator for consistency
+random.seed(0)
+
 # Endpoint configuration
 base_url = "http://vm-d69d62d7.test-server.ag"
 
 # Define number of users
-num_users = 10
+num_users = 1000
 
 # Store created users
 created_users = []
@@ -38,13 +41,20 @@ def create_user():
 def login_users():
     login_url = f"{base_url}/users/login"
 
-    for user in created_users:
+    print("Logging in all Users")
+
+    total = len(created_users)
+
+    for i, user in enumerate(created_users, start=1):
+        # Display progress indicator because it takes a while
+        print(f"Logged in: {i}/{total}", end='\r')
+
         # Log in the user
         login_payload = {"user": {"email": user["email"], "password": user["password"]}}
         response = requests.post(login_url, json=login_payload)
         
         if response.status_code != 200:
-            print(f"Failed to log in user {user['username']}")
+            print(f"\nFailed to log in user {user['username']}")
             continue
 
         # Extract the token from the response
@@ -55,22 +65,34 @@ def create_articles():
     article_url = f"{base_url}/articles"
 
     for user in created_users:
-        # Create an article
-        article_payload = {
-            "article": {
-                "title": fake.sentence(),
-                "description": fake.text(),
-                "body": fake.text(),
-                "tagList": [fake.word(), fake.word()]
+        num_articles = random.randint(0, 10)
+
+        for _ in range(num_articles):
+            # Create article description and select a random number of words from that as tags
+            article_description = fake.text()
+            words = article_description.split()
+            if len(words) < 10:
+                max_num_tags = len(words)
+            else:
+                max_num_tags = random.randint(1, 10)
+            tags = random.sample(words, max_num_tags)
+
+            # Create an article
+            article_payload = {
+                "article": {
+                    "title": fake.sentence(),
+                    "description": article_description,
+                    "body": fake.text(),
+                    "tagList": tags
+                }
             }
-        }
-        headers = {"Authorization": f"Token {user['token']}"}
-        response = requests.post(article_url, json=article_payload, headers=headers)
-        
-        if response.status_code == 200:
-            print(f"Article created successfully for user {user['username']}")
-        else:
-            print(f"Failed to create article for user {user['username']}")
+            headers = {"Authorization": f"Token {user['token']}"}
+            response = requests.post(article_url, json=article_payload, headers=headers)
+            
+            if response.status_code == 200:
+                print(f"Article created successfully for user {user['username']}")
+            else:
+                print(f"Failed to create article for user {user['username']}")
 
 def get_articles():
     articles_url = f"{base_url}/articles"
@@ -84,44 +106,37 @@ def get_articles():
     return articles
 
 def post_comments():
-    articles_url = f"{base_url}/articles"
-    articles_response = requests.get(articles_url)
-    
-    if articles_response.status_code != 200:
-        print("Failed to fetch articles")
-        return
-
-    articles = articles_response.json().get("articles", [])
+    articles = get_articles()
     
     if not articles:
         print("No articles found")
         return
 
-    # Seed the random number generator for consistency
-    random.seed(0)
-
     for user in created_users:
-        # Pick a random article
-        article = random.choice(articles)
-        slug = article["slug"]
-        token = user.get("token")
+        num_comments = random.randint(0, 10)
 
-        if not token:
-            print(f"User {user['username']} does not have a token, skipping...")
-            continue
+        for _ in range(num_comments):
+            # Pick a random article
+            article = random.choice(articles)
+            slug = article["slug"]
+            token = user.get("token")
 
-        # Post a comment to the random article
-        comment_url = f"{base_url}/articles/{slug}/comments"
-        headers = {"Authorization": f"Token {token}"}
-        comment_body = {"comment": {"body": fake.sentence()}}
-        
-        response = requests.post(comment_url, json=comment_body, headers=headers)
-        
-        if response.status_code != 201:
-            print(f"Failed to post comment for user {user['username']}")
-            continue
+            if not token:
+                print(f"User {user['username']} does not have a token, skipping...")
+                continue
 
-        print(f"User {user['username']} posted a comment on article {slug}")
+            # Post a comment to the random article
+            comment_url = f"{base_url}/articles/{slug}/comments"
+            headers = {"Authorization": f"Token {token}"}
+            comment_body = {"comment": {"body": fake.sentence()}}
+            
+            response = requests.post(comment_url, json=comment_body, headers=headers)
+            
+            if response.status_code != 201:
+                print(f"Failed to post comment for user {user['username']}")
+                continue
+
+            print(f"User {user['username']} posted a comment on article {slug}")
 
 def favorite_articles():
     articles = get_articles()
@@ -130,27 +145,30 @@ def favorite_articles():
         return
     
     for user in created_users:
-        token = user.get("token")
-        if not token:
-            print(f"User {user['username']} is not logged in")
-            continue
-        
-        # Pick a random article
-        random_article = random.choice(articles)
-        slug = random_article["slug"]
-        
-        # Construct the favorite article URL
-        favorite_url = f"{base_url}/articles/{slug}/favorite"
-        
-        # Set up the headers
-        headers = {"Authorization": f"Token {token}"}
-        
-        # Send the request to favorite the article
-        response = requests.post(favorite_url, headers=headers)
-        if response.status_code == 200:
-            print(f"User {user['username']} favorited article {slug}")
-        else:
-            print(f"Failed to favorite article {slug} for user {user['username']}")
+        num_favorites = random.randint(0, 10)
+
+        for _ in range(num_favorites):
+            token = user.get("token")
+            if not token:
+                print(f"User {user['username']} is not logged in")
+                continue
+            
+            # Pick a random article
+            random_article = random.choice(articles)
+            slug = random_article["slug"]
+            
+            # Construct the favorite article URL
+            favorite_url = f"{base_url}/articles/{slug}/favorite"
+            
+            # Set up the headers
+            headers = {"Authorization": f"Token {token}"}
+            
+            # Send the request to favorite the article
+            response = requests.post(favorite_url, headers=headers)
+            if response.status_code == 200:
+                print(f"User {user['username']} favorited article {slug}")
+            else:
+                print(f"Failed to favorite article {slug} for user {user['username']}")
 
 
 # Create users
